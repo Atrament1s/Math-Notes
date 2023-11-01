60d7ccd7a769d931adbfdb8f991b1478fada1857
Atrament1s <93157646+Atrament1s@users.noreply.github.com>
2023-11-01 15:53:51 -0500

    Quartz sync: Nov 1, 2023, 3:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "AP Calculus AB",
+    pageTitle: "Math Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
